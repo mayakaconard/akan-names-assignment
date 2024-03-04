ad994469aa7bace3a3bda4924cdd0cff9a5f9b22
mayakaconard <cototo@safaricom.co.ke>
2024-03-04 15:39:31 +0300

    Added Day function

--- index.js
+++ index.js
@@ -1,60 +1,114 @@
-function getDetails(){
+function getDetails() {
     const date = document.getElementById('date').value;
-    const gender= document.getElementById('gender').value;
+    const gender = document.getElementById('gender').value;
 
     alert('Date = '+ date + ' and Gender = '+gender);
-    const splitDate=date.split('-');
-    const year=splitDate[0];
-    const mm = splitDate[1];
-    const dd=splitDate[2]
 
-    const cc=year.slice(0,2);
-    const yy = year.slice(-2)
-    //alert(yy);
 
-    
 
-  const dayOfTheWeek=dayOfWeek(cc,yy,mm,'04')
+
+    const dayOfTheWeek = dayOfWeek(date)
     alert(dayOfTheWeek);
 
-    
+    const name = akanName(dayOfTheWeek, gender);
+    alert(name);
+
+
 }
 
-function dayOfWeek(CC, YY, MM, DD) {
+function dayOfWeek(date) {
+    const splitDate = date.split('-');
+    const year = splitDate[0];
+    const MM = parseInt(splitDate[1]);
+    const DD = parseInt(splitDate[2]);
+
+    const CC = parseInt(year.slice(0, 2));
+    let YY = parseInt(year.slice(-2));
+    alert(YY % 4);
+
+
+    const feb = validateFeb(MM, DD, YY);
+    alert(feb);
+
     // Adjust month and year for Zeller's formula
-    // if (MM < 3) {
-    //     MM += 12;
-    //     YY -= 1;
-    // }
+    if (MM < 3) {
+        MM += 12;
+        YY -= 1;
+    }
 
-const h = (DD + Math.floor((13*(MM+1))/5) + YY + Math.floor(YY/4) + Math.floor(CC/4) - 2*CC) % 7;
-    // Zeller's Congruence calculation
-   // const h = (DD + Math.floor((13*(MM+1))/5) + YY + Math.floor(YY/4) + Math.floor(CC/4) - 2*CC) % 7;
-//  const h = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7
 
-    // h will be in the range 0 to 6, so we map it to the corresponding day
-    const days = ["Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+    const ddValid = validateDay(DD);
+    alert(ddValid)
+    if (ddValid == true) {
+        // Zeller's Congruence calculation
+        const h = (DD + Math.floor((13 * (MM + 1)) / 5) + YY + Math.floor(YY / 4) + Math.floor(CC / 4) - 2 * CC) % 7;
 
-const dayOfWeek = days[(h + 7) % 7];
-    //return days[(h + 7) % 7]; // Adjusting for negative values of h
-    return days[(h + 7) % 7];
+        // Mapping h to corresponding day of the week
+        const days = ["Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+        return days[(h + 7) % 7]; // Adjusting for negative values of h
+    }
+    else {
+        return 'Invalid Birthday Date!!!';
+    }
 }
 
 
-// function dayOfWeek(CC, YY, MM, DD) {
-//     // Adjust month for Zeller's formula
-//     // if (MM < 3) {
-//     //     MM += 12;
-//     //     YY -= 1;
-//     // }
+function validateFeb(mm, dd, yy) {
+    const leapYear = yy % 4;
+    alert(mm)
+    if (mm == 2) {
 
-//     // Zeller's Congruence calculation with modulus operation
-//    // const h = (((((CC/4) - 2*CC - 1) + ((5*YY/4)) + ((26*(MM+1)/10)) + DD) % 7) + 7) % 7;
-//    const h = (((CC/4) -2*CC-1) + ((5*YY/4)) + ((26*(MM+1)/10)) + DD) % 7
+        if (dd <= 0 || dd > 29 && leapYear == 0) {
 
-//    alert(h);
-//     // Mapping h to corresponding day of the week
-//     const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+            return 'Kindly input the correct date for February (01-29)';
+        }
+        else if (dd <= 0 || dd > 28 && leapYear != 0) {
+            return 'Kindly input the correct date for February (01-28)';
+        }
+        else {
+            return true;
+        }
+    }
+    else {
+        return true;
+    }
 
-//     return days[(h + 7) % 7];
-// }
+}
+
+
+function validateDay(dd) {
+    if (dd <= 0 || dd > 31) {
+        return 'Kindly input the correct date. ranging from 1-31';
+
+    } else {
+        return true;
+    }
+}
+
+function akanName(day, gender) {
+
+    const names = {
+        "male": {
+            "Sunday": "Kwasi",
+            "Monday": "Kwadwo",
+            "Tuesday": "Kwabena",
+            "Wednesday": "Kwaku",
+            "Thursday": "Yaw",
+            "Friday": "Kofi",
+            "Saturday": "Kwame"
+        },
+        "female": {
+            "Sunday": "Akosua",
+            "Monday": "Adwoa",
+            "Tuesday": "Abenaa",
+            "Wednesday": "Akua",
+            "Thursday": "Yaa",
+            "Friday": "Afia",
+            "Saturday": "Ama"
+        }
+    };
+
+    const dayName = names[gender.toLowerCase()][day];
+    return dayName;
+
+}
